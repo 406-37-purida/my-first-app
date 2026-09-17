5e21ad88a178f29acb2c87df4a3c14eb980f218d
406-37-purida <54740@yupparaj.ac.th>
2026-09-17 21:37:47 +0700

    Update game.py

--- game.py
+++ game.py
@@ -47,8 +47,7 @@ def reset_game():
 # 📌 ฟังก์ชัน MessageBox (Dialog)
 # ----------------------------------------------------
 @st.dialog("📊 สรุปผลการเล่นเกม")
-def show_result_dialog(ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8, ans9, ans10)
-:
+def show_result_dialog(ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8, ans9, ans10):
     st.balloons()
     score = 0
 
