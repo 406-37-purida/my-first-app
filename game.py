79f48ac0e9d45d7be4281999b92b27e7e95a0bfa
406-37-purida <54740@yupparaj.ac.th>
2026-09-17 22:24:08 +0700

    Update game.py

--- game.py
+++ game.py
@@ -67,8 +67,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8, ans9, ans
         st.success("✅ ข้อ 1: ถูกต้อง")
         score += 1
     else:
-        st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')
-        💡เฉลย : sunflower")
+        st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}') เฉลย : sunflower")
 
     if u_ans2 == "sorry":
         st.success("✅ ข้อ 2: ถูกต้อง")
